dbcfa3bbd6a176c7e83d68dafc44bb2d8d7885d7
Realvinci <kutivinci@gmail.com>
2023-07-09 14:26:56 +0100

    Added pages

---
+++ pages/02_Data.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#import matplotlib
+#matplotlib.use('TkAgg')
+
+
+import matplotlib.pyplot as plt
+
+#from matplotlib import pyplot as plt
+
+pd.set_option('mode.chained_assignment', None)
+
+
+from keras.models import Sequential
+from keras.layers import Dense, SimpleRNN
+from keras.optimizers import RMSprop
+from keras.callbacks import Callback
+
+
+humidity = pd.read_csv("humidity.csv")
+temp = pd.read_csv("temperature.csv")
+pressure = pd.read_csv("pressure.csv")
+
+
+
+
+
+
+st.title("Data Used and data sources")
+
+st.markdown(
+    """
+     Large sets of data got from ______ data studied and used as parameter to train my RNN model includes data for Humidity,
+     Temperature, Pressure spaning from 2012-2017 and ranging various cities.
+    """
+)
+
+st.title("Humidity data")
+st.write(humidity)
+
+st.title("Temperature Data")
+st.write(temp)
+
+st.title("Pressure Data")
+st.write(pressure)
+
+
+humidity_SF = humidity[['datetime','San Francisco']]
+temp_SF = temp[['datetime','San Francisco']]
+pressure_SF = pressure[['datetime','San Francisco']]
+
+
+
+st.write("humidity san-franciso head shape",humidity_SF.shape)
+st.write("temperature san-franciso head shape",temp_SF.shape)
+st.write("pressure san-franciso head shape",pressure_SF.shape)
+
+st.write("How many NaN are there in the humidity dataset?",humidity_SF.isna().sum()['San Francisco'])
+st.write("How many NaN are there in the temperature dataset?",temp_SF.isna().sum()['San Francisco'])
+st.write("How many NaN are there in the pressure dataset?",pressure_SF.isna().sum()['San Francisco'])
+
+
+st.markdown(
+    """
+    Given the irregularities in our dataset we are extracting the first ten and the last ten to find out the the space
+    and work on data gotten using data for San-francisco as my test model for first case. 
+    """
+)
+hmhead=humidity_SF.head(10)
+
+hmtail=humidity_SF.tail(10)
+
+
+st.write(hmhead)
+st.write(hmtail)
---
+++ pages/03_Trainplot.py
@@ -0,0 +1,66 @@
+import streamlit as st
+st.set_option('deprecation.showPyplotGlobalUse', False)
+import pandas as pd
+import numpy as np
+
+#import matplotlib
+#matplotlib.use('TkAgg')
+
+
+import matplotlib.pyplot as plt
+#import matplotlib as pl
+pd.set_option('mode.chained_assignment', None)
+
+
+
+from keras.models import Sequential
+from keras.layers import Dense, SimpleRNN
+from keras.optimizers import RMSprop
+from keras.callbacks import Callback
+
+
+humidity = pd.read_csv("humidity.csv")
+temp = pd.read_csv("temperature.csv")
+pressure = pd.read_csv("pressure.csv")
+
+humidity_SF = humidity[['datetime','San Francisco']]
+temp_SF = temp[['datetime','San Francisco']]
+pressure_SF = pressure[['datetime','San Francisco']]
+
+Tp = 700
+
+
+st.title("TrainPlots for Temperature,Humidity and Pressure")
+
+st.markdown(
+    """
+     This are the train plot gotten from the program when the data sets were pass through the funtion
+     With my Tp = 700
+    """
+)
+def plot_train_points(quantity='humidity',Tp=7000):
+    plt.figure(figsize=(10,5))
+    if quantity=='humidity':
+        plt.title("Humidity of first {} data points".format(Tp),fontsize=30)
+        plt.plot(humidity_SF['San Francisco'][:Tp],c='k',lw=1)
+    if quantity=='temperature':
+        plt.title("Temperature of first {} data points".format(Tp),fontsize=30)
+        plt.plot(temp_SF['San Francisco'][:Tp],c='k',lw=1)
+    if quantity=='pressure':
+        plt.title("Pressure of first {} data points".format(Tp),fontsize=30)
+        plt.plot(pressure_SF['San Francisco'][:Tp],c='k',lw=1)
+    plt.grid(True)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.show()
+
+plot_train_points('humidity')
+st.pyplot()
+    
+
+plot_train_points('temperature')
+st.pyplot()
+
+plot_train_points('pressure')
+
+st.pyplot()
